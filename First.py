12511a21247fad07daf58c4150b3322c1217f035
Arpita Sah <arpitasah750@gmail.com>
2025-06-22 23:28:58 +0545

    First code in Python Programming Language
    Here, I used the print() to print word and start my first day in python programming language.

---
+++ First.py
@@ -0,0 +1,11 @@
+print("Hello Workld!!") # In the python programming language print is used to print the written word or sentence insude the print("") or print('')
+
+# This is the starting of my python code
+
+print('''Hey!!
+      I am Arpita Sah.
+      Today I am step my fist step in the python coding.
+      And wel-comming Everyone in Arpita Coding World!!
+      With quotation,"Always keep hope in your downfall and realize the mistake and come up with better achievement in your life".
+      Finally, Learn my your mistake rather than ignoring them.''')
+# Here I used print(''' ''') to print out the multiple lines.
