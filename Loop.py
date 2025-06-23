bab340aef0971735b1b8b39bf97d77dbb7d93467
Arpita Sah <arpitasah750@gmail.com>
2025-06-23 23:02:47 +0545

    Add files via upload

---
+++ Loop.py
@@ -0,0 +1,36 @@
+#range(5)
+#start=0
+#condition<5
+#increment 1
+# 0 1 2 3 4
+
+#range(1,6)
+#start =1
+# condition<6
+#increment 1
+# 1 2 3 4 5
+
+#range(1,6,2)
+#start=1
+#condition<6
+#increment 2
+# 1 3 5
+
+# For single parameter in range.
+for n in range(5):
+    print(n)
+print()
+
+# For two parameter in range.
+for a in range(1,7):
+    print(a)
+print()
+
+# For three parameter in range.
+for n in range(1,9,2):
+    print(n)
+print()
+
+for a in range(1,11):
+    print("2*",a,"=",2*a)
+print()
