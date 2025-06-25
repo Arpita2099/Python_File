756c8a50768ede473b38ac61aed2776872b8346d
Arpita Sah <arpitasah750@gmail.com>
2025-06-25 18:41:51 +0545

    List_Iteration.py

---
+++ List_Iteration.py
@@ -0,0 +1,22 @@
+l=[10,20,30,40,50,60]
+t=len(l)
+for n in range(t):
+    print(l[n])
+
+for n in range(t-1,-1,-1):
+    print(l[n])
+print(t)
+for a in range(t):
+    print(a)
+    print(l[a])
+
+# Direct list passing element to print all elements of list.
+print("")
+for a in l:
+    print(a)
+
+    print("")
+for a in range(t-1,-1,-1):
+    print(a)
+
+
