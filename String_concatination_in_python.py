302942d07fbd029c461996d1bcb23881f3f0e7d1
Arpita Sah <arpitasah750@gmail.com>
2025-07-27 08:29:36 +0545

    Create String_concatination_in_python.py
    How + is used to add the number or string in the python.

---
+++ String_concatination_in_python.py
@@ -0,0 +1,10 @@
+# + is used to add the number, also is used to add two string together. For instance,
+a="Hello "
+b="Arpita Sah"
+print(a+b)
+
+x=30
+y=45
+print(x+y)
+
+# We have to note one thing that when you want to add one number and other string uning + it will show the error in  the code. Therefore, add the same either number or string using +.
