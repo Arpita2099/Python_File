a622019a9f69a10d17eca9c78beafec56427b0fa
Arpita Sah <arpitasah750@gmail.com>
2025-07-27 06:58:28 +0545

    Create first_hello.py
    In this code, we can get the using of print() function in the python. The purpose of using python programming language is that python is the interpreted, object-orientated, high level programming language with dynamic sematic. By using print() function anybody can print the normal calculation, can print the single or multiple statement at using the one print() function in the python. In additional, print() function is use to compare. That all about this code.

---
+++ first_hello.py
@@ -0,0 +1,14 @@
+# This is my first python program where I am printing the output in the Python like one statement and multiple statement output using print() function.
+# This is the Single statement which I am writing using print() function.
+print("Hello Everyone!! Welcome to the Arpita Python coding world.")
+
+# This is the multiple statement using print() function.
+print("Hello Everyone!!"," Once again I started learning the python in the same strength"," I hope this time I will not quit in the middle due to my college assignment."," I will do hard work and manage my regular codung day in the github.")
+
+# Even you can compare and also can do the normal calculation using the print() function in python.
+#For instance,
+a=4
+b=5
+print(b>a)
+print(a==b)
+print(a+b)
