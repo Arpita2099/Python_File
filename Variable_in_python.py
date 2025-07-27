7793ea0a57272fe74512dc5a97f49c010a436fd4
Arpita Sah <arpitasah750@gmail.com>
2025-07-27 07:37:38 +0545

    Create Variable_in_python.py
    Variable in the python is the specific name give to anything to print same variable name in all code easily. While starting the variable name we have to focused on some rule like do not start variable name using any number, special character, it will show the error. That all about the variable.

---
+++ Variable_in_python.py
@@ -0,0 +1,13 @@
+# Here, we are leatrning to using variable in python. 
+num1=10
+num2=9
+print(num1,num2)
+
+# The id is use in the python to find the memory address or memory allocation of the variable. For instance,
+print(id(num1),id(num2))
+
+# Also, if the variable have same value than their  memeory allocation is also same. For instance,
+x=12
+y=12
+print(id(x),id(y))
+
