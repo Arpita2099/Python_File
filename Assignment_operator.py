da6d051208d67293d0f8e49b9c35f270a6239e62
Arpita Sah <arpitasah750@gmail.com>
2025-07-27 08:40:47 +0545

    Create Assignment_operator.py

---
+++ Assignment_operator.py
@@ -0,0 +1,22 @@
+# + operator is used to add the numbers. For instance,
+a=50
+b=25
+print(a+b)
+
+# - operator is used to substract the numbers. For instance,
+print(a-b)
+
+# * operator is used to multiple the numbers. For instance,
+print(a*b)
+
+# / operator is used to divide the numbers. For instance,
+print(a/b)
+
+# ** operator is used to find out the exponents of nay number. For inastance,
+# Find out the square and cube of the number
+print(10**2) # Square of number 10
+print(2**3) # cube of number 2
+
+# // operators is used to give only the integer number. For instance,
+print(20//3) # It will give the output only 6.
+
