9a8b376dc2422e157f0f2c8e02f8165313faf910
Arpita Sah <arpitasah750@gmail.com>
2025-07-27 09:18:03 +0545

    Create Comparison_operators.py

---
+++ Comparison_operators.py
@@ -0,0 +1,19 @@
+a=18
+b=20
+# == operator is use for comparing equal.
+print(a==b)
+
+# != shows not equal.
+print(a!=b)
+
+# > is greater than.
+print(a>b)
+
+# < is smaller than.
+print(a<b)
+
+# >= is greater than equal to.
+print(a>=b)
+
+# <= is smaller than equal to.
+print(a<=b)
