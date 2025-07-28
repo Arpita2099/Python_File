31510be299b3f40d252080a382e6b72a791c4ca5
Arpita Sah <arpitasah750@gmail.com>
2025-07-28 23:27:46 +0545

    Create Sequence_Datatype.py

---
+++ Sequence_Datatype.py
@@ -0,0 +1,10 @@
+# string
+str="My name is Arpita Sah and I am passinated about the Python developer!!"
+print(str, type(str))
+
+str1='''
+Hey, Everyone!!
+Come tonight in my frame
+To have fun and party tonight.
+'''
+print(str1, type(str1))
