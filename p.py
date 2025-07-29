1bd205ff7bbde24a29bd642d20bd07cf632a4814
Arpita Sah <arpitasah750@gmail.com>
2025-07-30 00:02:15 +0545

    Add files via upload

---
+++ p.py
@@ -0,0 +1,163 @@
+# This is my first python program where I am printing the output in the Python like one statement and multiple statement output using print() function.
+# This is the Single statement which I am writing using print() function.
+print("Hello Everyone!! Welcome to the Arpita Python coding world.")
+
+# This is the multiple statement using print() function.
+print("Hello Everyone!!"," Once again I started learning the python in the same strength"," I hope this time I will not quit in the middle due to my college assignment", " I will do hard work and manage my regular codung day in the github.")
+
+a=4
+b=5
+print(b>a)
+print(a==b)
+print(a+b)
+
+# Here, we are leatrning to using variable in python. 
+num1=10
+num2=9
+print(num1,num2)
+
+# The id is use in the python to find the memory address or memory allocation of the variable. For instance,
+print(id(num1),id(num2))
+
+# Also, if the variable have same value than their  memeory allocation is also same. For instance,
+x=12
+y=12
+print(id(x),id(y))
+
+# + is used to add the number, also is used to add two string together. For instance,
+a="Hello"
+b="Arpita Sah"
+print(a+b)
+
+x=30
+y=45
+print(x+y)
+
+
+# + operator is used to add the numbers. For instance,
+a=50
+b=25
+print(a+b)
+
+# - operator is used to substract the numbers. For instance,
+print(a-b)
+
+# * operator is used to multiple the numbers. For instance,
+print(a*b)
+
+# / operator is used to divide the numbers. For instance,
+print(a/b)
+
+# ** operator is used to find out the exponents of nay number. For inastance,
+# Find out the square and cube of the number
+print(10**2) # Square of number 10
+print(2**3) # cube of number 2
+
+# // operators is used to give only the integer number. For instance,
+print(20//3) # It will give the output only 6.
+
+# = operator is use for assign any value.
+x=15
+print(x)
+
+# += operator is use for addition.
+x+=3
+print(x)
+
+# -= operator is use for substraction.
+x-=5
+print(x)
+
+a=18
+b=20
+# == operator is use for comparing equal.
+print(a==b)
+
+# != shows not equal.
+print(a!=b)
+
+# > is greater than.
+print(a>b)
+
+# < is smaller than.
+print(a<b)
+
+# >= is greater than equal to.
+print(a>=b)
+
+# <= is smaller than equal to.
+print(a<=b)
+
+
+# and operator
+x=10
+y=20
+print(x<5 and y>10) 
+
+# or operator
+x=10
+y=20
+print(x<5 or y>10) 
+
+# not operator
+x=10
+y=20
+print(not x!=5) 
+
+# and operator
+x=10
+y=20
+print(x<5 and y>10) 
+
+# or operator
+x=10
+y=20
+print(x<5 or y>10) 
+
+# not operator
+x=10
+y=20
+print(not x!=5) 
+
+string="Arpita"
+print('A' in string)
+print('s' in string)
+
+list=[10,20,30,40,50]
+print(100 in list)
+print(30 in list)
+
+# is operators- Return true if both variables are the same object.
+p=100
+q=100
+print(p is q, p==q)
+print( p is not q, p!=q) # It will give false.
+
+x=10
+y=9
+print(bin(x&y))
+print(bin(x|y))
+print(bin(x^y))
+
+#integer
+x=10
+print(x, type(x))
+
+#float
+Y=20.10
+print(y, type(y))
+
+#complex
+z=3+2j
+print(z, type(z))
+
+# string
+str="My name is Arpita Sah and I am passinated about the Python developer!!"
+print(str, type(str))
+
+str1='''
+Hey, Everyone!!
+Come tonight in my frame
+To have fun and party tonight.
+'''
+print(str1, type(str1))
