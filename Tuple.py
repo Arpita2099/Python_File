760836d371630c30ee47adefb8393af2ab4a752c
Arpita Sah <arpitasah750@gmail.com>
2025-08-05 20:59:57 +0545

    Create Tuple.py
    Tuple is an order sequence of items same as a list.
    It is defined within parenthesis() where items are separated by commas.
    For instance: t=(5, "Arpita", 1-4j)

---
+++ Tuple.py
@@ -0,0 +1,7 @@
+# Tuple is an order sequence of items same as a list.
+# It is defined within parenthesis() where items are separated by commas.
+# For instance: t=(5, "Arpita", 1-4j)
+
+T=(1,2,"Sah",5+4j)
+print(T) # It will Provide output: (1,2,"Sah",(5+4j))
+print(type(T)) # It will provide output: <class 'tuple'>
