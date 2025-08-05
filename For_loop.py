74d2e7322bc1e6920154dd1b6f00f019d36ea044
Arpita Sah <arpitasah750@gmail.com>
2025-08-05 22:18:20 +0545

    Create For_loop.py

---
+++ For_loop.py
@@ -0,0 +1,14 @@
+# range(4) : It denoted that the condition started by 0 and increment by +1 and print less than 5 number.
+
+# range(1,6) : It denoted that, the condition is started from 1 and increment by +1 and print number continuosly less than 6 number.
+
+# range(2, 16, 2) : It denoted that, the condition is started from 2, increment by +2, print the number less than 16 as output i.e 14.
+
+for n in range(4):
+  print(n)
+
+for n in range(1,6):
+  print(n)
+
+for n in range(2,16,2):
+  print(n)
