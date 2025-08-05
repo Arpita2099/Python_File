5ddfe54a25b58583f6eced22c25e9f1c1a4f48f1
Arpita Sah <arpitasah750@gmail.com>
2025-08-05 21:17:35 +0545

    Create Set.py
    Set is an unordered collection of items.
    Every set element is unique (no duplicate value).
    Set is immutable value (can not be changed).

---
+++ Set.py
@@ -0,0 +1,6 @@
+# Set is an unordered collection of items.
+# Every set element is unique (no duplicate value).
+# Set is immutable value (can not be changed).
+
+s={10,20,30,40,50}
+print(s,type(s)) # It will be output: {50, 20, 40, 10, 30} <class 'set'>
