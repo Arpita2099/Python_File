8ee04b6068628807847dc65cfd8c96ee14d8fdb8
Arpita Sah <arpitasah750@gmail.com>
2025-08-05 21:39:15 +0545

    Create User_input_and_Type_casting.py
    Here, the user can take input using input().

---
+++ User_input_and_Type_casting.py
@@ -0,0 +1,11 @@
+# User can input like bellow:
+Str=input("Enter Your Name here: ")
+print(Str) #It will give output: Firstly it will ask Enter Your Name here: and you have to write what every you want output and it will print that.
+
+# User can input the type casting bellow:
+Num1=int(input("Enter the first Number: "))
+Num2=int(input("Enter the Second Number: "))
+print("Adding both the number we get: ",Num1+Num2)
+
+
+
