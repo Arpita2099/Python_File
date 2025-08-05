3e8838a8a32da1044076cb8b417a3e99cc45c10d
Arpita Sah <arpitasah750@gmail.com>
2025-08-05 21:08:59 +0545

    Create Dictionary.py
    Dictionary is an unordered collection of key_value pairs.
    In the python, dictionary ids defined within the {} with each item being a pair in the form of key:value.

---
+++ Dictionary.py
@@ -0,0 +1,8 @@
+# Dictionary is an unordered collection of key_value pairs.
+# In the python, dictionary ids defined within the {} with each item being a pair in the form of key:value.
+
+d={"Name":"Arpita",
+  'Last_name':"Sah"
+  }
+print(d) # It will give output: {'Name': 'Arpita', 'Last_name': 'Sah'} 
+print(type(d)) # It will give output: <class 'dict'>
